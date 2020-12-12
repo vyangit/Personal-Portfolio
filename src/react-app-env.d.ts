f5fc80ebffbb4115ae73b9794020ae0e74d5a077
Victor Yan <vyan.git@gmail.com>
2020-12-12 02:36:09 -0800

    Added navigation bar. Added logo and lang menu placeholders.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="react-scripts" />
+/// <reference types="react-dom"/>
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
--- src/ts/ui/components/NavBar.component.tsx
+++ src/ts/ui/components/NavBar.component.tsx
@@ -2,21 +2,35 @@ import React, {Component} from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
 import MenuIcon from '@material-ui/icons/Menu'
+
+import SignatureLogo from '../../../assets/logo.svg'
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles'
 import PropTypes from 'prop-types'
 
-class NavBarComponent extends Component {
-    render() {
-        return <div>
-            <AppBar position='static'>
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-        </div>
-    }
-}
+const useStyles = makeStyles((theme: Theme) => 
+    createStyles({
+        signatureLogo: {
+            flexGrow: 1,
+        }
+    })
+)
 
-export default NavBarComponent
+export default function NavBarComponent() {
+    const navBarStyles = useStyles()
+    
+    return <div>
+        <AppBar position='static'>
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon/>
+                </IconButton>
+                <div className={navBarStyles.signatureLogo}>
+                    <img src={SignatureLogo} height="60"></img>
+                </div>
+                <Button color="inherit">en-CA</Button>
+            </Toolbar>
+        </AppBar>
+    </div>
+}
