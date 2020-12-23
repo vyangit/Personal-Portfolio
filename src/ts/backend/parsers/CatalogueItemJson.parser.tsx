211ed893e34134faa81497fa17ef6b203a63ba95
Victor Yan <vyan.git@gmail.com>
2020-12-23 00:01:22 -0800

    Added logic for parsing

---
+++ src/ts/backend/parsers/CatalogueItemJson.parser.tsx
@@ -0,0 +1,25 @@
+import CatalogueItemModel from '../../models/CatalogueItem.model';
+
+export default class CatalogueItemJsonParser {
+    public static parseJson(catalogueJson: string): Array<CatalogueItemModel> {
+        var catalogueMetaData = JSON.parse(catalogueJson);
+        var results = new Array<CatalogueItemModel>();
+        if (catalogueMetaData.hasOwnProperty('catalogue')) {
+            catalogueMetaData['catalogue'].forEach((entry: any) => {
+                results.push(new CatalogueItemModel(
+                    entry.title as string,
+                    entry.description as string,
+                    entry.thumbnailSrc as string,
+                    entry.filterTags as string[],
+                    new Date(entry.releaseDate as string),
+                    entry.releaseLink as string,
+                    entry.repoLink as string
+                ));
+            });
+        }
+
+        return results;
+    }
+}
+
+export {}
