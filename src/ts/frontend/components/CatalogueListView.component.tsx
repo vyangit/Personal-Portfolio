a35a5eeb701b7f4b2489b5739d682d54f144df57
Victor Yan <vyan.git@gmail.com>
2020-12-24 22:43:04 -0800

    Added starter template for app catalogue list view

--- src/ts/frontend/components/CatalogueListView.component.tsx
+++ src/ts/frontend/components/CatalogueListView.component.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import CatalogueItemModel from '../../models/CatalogueItem.model'
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography'
+
 interface CatalogueListViewComponentProps {
     items: Array<CatalogueItemModel>
 }
 
 export default function CatalogueListViewComponent(props: CatalogueListViewComponentProps) {
-    // TODO: Implement
-    return (<div>CatalogueListViewComponent</div>)
+    const renderCatalogueItems = () => {
+        let catalogueListItems = [];
+        for (let i= 0; i < props.items.length; i++) {
+            let item: CatalogueItemModel = props.items[i];
+            catalogueListItems.push(
+            <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                    <Avatar variant="square" alt={item.title} src={item.thumbnailSrc}/>
+                </ListItemAvatar>
+                <ListItemText
+                    primary={item.title}
+                    secondary={item.description}
+                    />
+            </ListItem>)
+        }
+        return catalogueListItems.length == 0 ? null : catalogueListItems
+    }
+    
+    return (
+    <List>
+        {renderCatalogueItems()}
+    </List>);
 }
--- src/ts/frontend/pages/Catalogue.page.tsx
+++ src/ts/frontend/pages/Catalogue.page.tsx
@@ -12,7 +12,6 @@ import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Autocomplete from '@material-ui/lab/Autocomplete';
