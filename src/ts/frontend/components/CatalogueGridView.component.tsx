801eccc8fb5321d2aebcfffaaa4ab6fd3ff45975
Victor Yan <vyan.git@gmail.com>
2020-12-25 20:25:35 -0800

    Added grid list col resizing logic.

--- src/ts/frontend/components/CatalogueGridView.component.tsx
+++ src/ts/frontend/components/CatalogueGridView.component.tsx
@@ -5,11 +5,30 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
+import {makeStyles} from '@material-ui/core/styles'
+
 interface CatalogueGridViewComponentProps {
     items: Array<CatalogueItemModel>
 }
 
+const gridItemWidth = 300;
+    
+const determineNumCols = () => {
+    let cols = Math.floor(window.screen.width/gridItemWidth);
+    if (cols < 1) cols = 1;
+    return cols;
+}
+
+const useStyles = makeStyles({
+    gridList: {
+        flexGrow: 1,
+        flexWrap: 'wrap'
+    }
+})
+
 export default function CatalogueGridViewComponent(props: CatalogueGridViewComponentProps) {
+    const classes = useStyles();
+
     const renderCatalogueItems = () => {
         let catalogueListItems = [];
         for (let i= 0; i < props.items.length; i++) {
@@ -24,9 +43,9 @@ export default function CatalogueGridViewComponent(props: CatalogueGridViewCompo
         }
         return catalogueListItems.length == 0 ? null : catalogueListItems
     }
-    
+
     return (
-    <GridList cellHeight={180}>
+    <GridList cellHeight={200} cols={(determineNumCols())} className={classes.gridList}>
         {renderCatalogueItems()}
     </GridList>);
 }
